34f4a15d91c5c4055e7b39c8622f63bcfd613931
sfc-gh-tpetrache <tatiana.petrache@snowflake.com>
2022-05-18 16:54:06 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ def get_fruityvice_data(this_fruit_choice):
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
   
-
+  
 import requests
 streamlit.header("Fruit Advice!")
 try:
@@ -45,17 +45,22 @@ except URLError as e:
 
 
 
-streamlit.stop()
+
 import snowflake.connector
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_row = my_cur.fetchall()
-streamlit.text("Hello from Snowflake:")
-streamlit.dataframe(my_data_row)
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("SELECT * from fruit_load_list")
+    return my_cur.fetchall()
+    
+if streamlit.button('get fruit list'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_row = get_fruit_load_list():
+  streamlit.text("Hello from Snowflake:")
+  streamlit.dataframe(my_data_row)
 
 
+streamlit.stop()
 add_my_fruit = streamlit.text_input("add your fruit", 'Kiwi')
 streamlit.write('thanks for adding', add_my_fruit)
 
