eb55e8621827ccd94ae9d1c08e645c5f75f7ca18
sfc-gh-tpetrache <tatiana.petrache@snowflake.com>
2022-05-18 15:37:19 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,9 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.header("Fruit Advice!")
+fruit_choice = streamlit.text_input("What fruit would you like to know info about?", 'Kiwi')
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #streamlit.text(fruityvice_response.json())
 
 
